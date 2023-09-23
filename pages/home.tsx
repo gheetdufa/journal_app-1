0ba5efbc0e5ac0b83d97d7eefeb19b2d45d77cd9
yobed <100246424+yobed@users.noreply.github.com>
2023-09-23 15:00:16 -0400

    Commit

---
+++ pages/home.tsx
@@ -0,0 +1,15 @@
+// @ts-ignore
+import { Navbar } from '../components/Navbar';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+
+
+
+export default function Home(){
+    return (
+        <div>
+            <div className='rounded-lg card border-2 shadow-card bg-whitep-4 w-1/3 m-6 text-center'>
+            <h1 className='flex flex-row justify-center'>How are you?</h1>
+            </div>
+        </div>
+    )
+}
